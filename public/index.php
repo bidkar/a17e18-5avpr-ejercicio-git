8908e297a1356767fca367c98a864741ab30273b
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2017-09-18 08:37:42 -0600

    Metodo all()
    Se creo el medoto all() para obtener todos los registros de la tabla users

--- public/index.php
+++ public/index.php
@@ -3,11 +3,11 @@ require_once '../src/app/db/mysql.php';
 require_once '../src/models/user.php';
 use Models\User;
 
-$usuario = new User();
+// $usuario = new User();
 // var_dump($usuario->FindById(1));
 // 1: error en la consulta, 2: bool false, 3: object Models\User
-$id = 1;
-$resultado = $usuario->FindById($id);
+$id = 10;
+$resultado = User::FindById($id);
 if ($resultado == false) {
     // imprimir que no encontro al usuario
     echo "El usuario con id=$id no fue encontrado.";
--- src/models/user.php
+++ src/models/user.php
@@ -20,7 +20,7 @@ class User {
             $this->data[$var] = $value;
     }
 
-    public function FindById($id) {
+    public static function FindById($id) {
         $cnn = new MySQL();
         $sql = sprintf("SELECT * FROM users WHERE id=%d",$id);
         $rst = $cnn->query($sql); // mysqli_result::fetch_assoc()
@@ -41,4 +41,28 @@ class User {
         }
     }
 
+    public function all() {
+        $cnn = new MySQL();
+        $sql = "SELECT id,name,firstname,lastname,email FROM users";
+        $rst = $cnn->query($sql);
+        $cnn->close();
+
+        if (!$rst) {
+            die('Error en la consulta');
+        } elseif ($rst->num_rows == 0) {
+            return false;
+        } else {
+            $usuarios = array();
+            while ($r = $rst->fetch_assoc()) {
+                $usuario = new User();
+                $usuario->id = $r['id'];
+                $usuario->name = $r['name'];
+                $usuario->firstname = $r['firstname'];
+                $usuario->lastname = $r['lastname'];
+                $usuario->email = $r['email'];
+                array_push($usuarios, $usuario);
+            }
+            return $usuarios;
+        }
+    }
 }
