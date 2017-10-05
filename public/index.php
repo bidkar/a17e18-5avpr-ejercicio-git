73d87609d31efc701949a308ae9cdfc0cdcf8a5c
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2017-10-05 10:53:57 -0600

    Bootstrap + Vistas
    Se agregó bootstrap y las vistas, también se probó que funcionen las vistas con la configuración previa del .htaccess

--- public/index.php
+++ public/index.php
@@ -1,10 +1,20 @@
 <?php
 require_once '../config.inc.php';
 require_once '../src/app/autoload.php';
-use Src\Models\User;
 
-echo 'APP_DIR = ' . APP_DIR . '<br>';
-echo '__DIR__ = ' . __DIR__;
-// src/models/user.php
 
-$a = new User();
+if (isset($_GET['url'])) {
+    switch ($_GET['url']) {
+        case 'home':
+            include '../src/views/home.php';
+            break;
+        case 'login':
+            include '../src/views/login.php';
+            break;
+        default :
+            echo '404';
+            break;
+    }
+} else {
+    include '../src/views/home.php';
+}
